b9defc263ab5344b116105d5a8e17657db41a998
Arbousied <kiger.maxence@gmail.com>
2019-02-01 00:49:21 +0100

    JPP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 10.times do
-  doctor = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, specialty: Faker::Dessert.variety, zip_code: Faker::Number.number(5) )
+  city = City.create!(name: Faker::Address.city)
 end
 
 10.times do
-  patient = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  doctor = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,city_id: City.all.sample.id, zip_code: Faker::Number.number(5) )
 end
 
 10.times do
-  appointment = Appointment.create!(date: Faker::Date.forward(rand(1..364)), doctor_id: (rand(Doctor.first.id..Doctor.last.id)), patient_id: (rand(Patient.first.id..Patient.last.id)))
+  patient = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,city_id: City.all.sample.id)
+end
+
+10.times do
+  appointment = Appointment.create!(date: Faker::Date.forward(rand(1..364)), doctor_id: (rand(Doctor.first.id..Doctor.last.id)), patient_id: (rand(Patient.first.id..Patient.last.id)), city_id: City.all.sample.id)
+end
+
+10.times do
+  specialty = Specialty.create!(name: Faker::Gender.type)
+end
+
+10.times do
+  doctorspecialty = DoctorSpecialty.create!(doctor_id: Doctor.all.sample.id, specialty_id: Specialty.all.sample.id)
 end
